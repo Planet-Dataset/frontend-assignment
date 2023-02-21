94f9909f91b6c1c09ff859307257b2032425342e
guillecg98 <guillecosano42@gmail.com>
2023-02-21 13:16:03 +0100

    feat: Added pagination in fetch all posts.

--- services/api/src/posts/application/get-posts.ts
+++ services/api/src/posts/application/get-posts.ts
@@ -1,14 +1,19 @@
 import { IPostsRepository } from "@app/posts/domain";
 import { FilePostsRepository } from "@app/posts/infrastructure";
 
-
 export class GetPosts {
   private postsRepository: IPostsRepository;
   constructor({ postsRepository = new FilePostsRepository() }) {
     this.postsRepository = postsRepository;
   }
 
-  async execute() {
-    return await this.postsRepository.findAllPosts();
+  async execute(page: number, limit: number) {
+    const data = await this.postsRepository.findAllPosts();
+    data.data.children = data.data.children.slice(
+      (page - 1) * limit,
+      page * limit
+    );
+
+    return { data: data.data.children, page, limit };
   }
 }
